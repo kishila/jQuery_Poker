ff607a5ebb1d87c1b4e3bcd6c08ffad28ca2639e
kishila <s12t282@stmail.eng.kagawa-u.ac.jp>
2015-09-23 23:19:40 +0900

    jQueryファイルを外部ファイル化

---
+++ js/import.js
@@ -0,0 +1,1 @@
+document.write('<script type="text/javascript" src="./js/poker.js"></script>'); 
---
+++ js/poker.js
@@ -0,0 +1,44 @@
+
+//--- 開始ボタンが押されたときの処理
+$("#start").click(function() {
+  card_init();
+
+  //--カードの初期化
+  function card_init() {
+    var num = ["","","","",""];
+    var newly_flag;
+
+    // カードの数字を決定
+    for(i = 0; i < 5;i++) {
+      do {
+        num[i] = num_rand()
+        newly_flag = new_num_check(num, i);
+      } while(!newly_flag);
+    }
+    // カードの初期化
+    for(i = 0;i < 5;i++){
+      $("#card" + i).attr("src","./CardImage/" + num[i] + ".gif");
+    }
+  }
+
+  //-- 0～51の乱数を生成
+  function num_rand() {
+    var val = Math.round( Math.random()*51 );
+    if (val  < 10) {
+      str = "0" + val.toString();
+    } else {
+      str = val.toString();
+    }
+    return str;
+  }
+
+  //-- 被っていない数字か判断
+  function new_num_check(num, i) {
+    for(j=i-1;j>=0;j--){
+      if(num[i] == num[j]) {
+        return false;
+      }
+    }
+    return true;
+  }
+});
