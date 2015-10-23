83b6ba8eeb48d98a916a046d38fcc9b40aa2d6a6
kishila <s12t282@stmail.eng.kagawa-u.ac.jp>
2015-10-24 00:35:17 +0900

    カードの交換を実装

--- js/poker.js
+++ js/poker.js
@@ -13,7 +13,7 @@ $(document).ready( function(){
 $("#start").click(function() {
   card_init();            // カードの初期化
   button_visualization(); // ボタンの可視化
-  card_class_change();    // カードを操作対象に
+  card_operational();     // カードを操作対象に
   add_hover_fanc();       // 操作対象にオンマウス機能を追加
   add_click_fanc();       // 操作対象にクリック時の機能を追加
 
@@ -24,8 +24,8 @@ $("#start").click(function() {
     // カードの数字を決定
     for(i = 0; i < 5;i++) {
       do {
-        num[i] = num_rand()
-        newly_flag = new_num_check(num, i);
+        num[i] = num_rand();
+        newly_flag = new_num_check(i);
       } while(!newly_flag);
     }
     // カードの変更
@@ -46,7 +46,7 @@ $("#start").click(function() {
   }
 
   //-- 被っていない数字か判断
-  function new_num_check(num, i) {
+  function new_num_check(i) {
     for(j=i-1;j>=0;j--){
       if(num[i] == num[j]) {
         return false;
@@ -55,8 +55,8 @@ $("#start").click(function() {
     return true;
   }
 
-  //-- カードにクラスを追加、操作対象に
-  function card_class_change() {
+  //-- カードを操作対象に
+  function card_operational() {
     for(i = 0;i < 5;i++){
       $("#card" + i).addClass("operational");
     }
@@ -105,8 +105,12 @@ $("#start").click(function() {
 $('#change').click(function() {
   $("#change").hide();
   $("#restart").show();
-  remove_hover_fanc();       // 操作対象にオンマウス機能を追加
-  card_class_change();
+  remove_hover_fanc();       // 操作対象のオンマウス機能を削除
+  card_unoperational();      // カードを操作対象外に
+  card_reposit();            // カードを初期位置に戻す
+  card_change();             // 選択されたカードをチェンジ
+  card_judge();              // 役の判断
+
 
   //-- 操作対象からオンマウス機能を削除
   function remove_hover_fanc() {
@@ -115,11 +119,69 @@ $('#change').click(function() {
     $('.operational').off("click");
   }
 
-  //-- カードからクラスを削除
-  function card_class_change() {
+  //-- カードを操作対象外に
+  function card_unoperational() {
     for(i = 0;i < 5;i++){
       $("#card" + i).removeClass("operational");
     }
+  }
+
+  //-- カードを初期位置に戻す
+  function card_reposit() {
+    for(i = 0;i < 5;i++){
+      $("#card" + i).animate({'top':'0px'}, 0);
+    }
+  }
+
+  //-- 選択されたカードをチェンジ
+  function card_change() {
+    var usedCard = new Array();
+    var newly_flag;
+    var tmp;
+
+    //- 表示されている5枚のカードを記憶
+    for(i = 0; i < num.length; i++) {
+      usedCard[i] = num[i];
+    }
+    //- 選択されているカードの交換
+    for(k = 0;k < 5;k++){
+      if($("#card" + k).hasClass('selected')) {
+        do {
+          tmp = num_rand();
+          newly_flag = new_num_check(tmp, usedCard);
+        } while(!newly_flag)
+        num[k] = tmp;
+        $("#card" + k).attr("src","./CardImage/" + num[k] + ".gif");
+        usedCard.push(num[k]);
+        $("#card" + k).removeClass("selected");
+      }
+    }
+  }
+
+  //-- 0～51の乱数を生成
+  function num_rand() {
+    var val = Math.round( Math.random()*51);
+    if (val  < 10) {
+      str = "0" + val.toString();
+    } else {
+      str = val.toString();
+    }
+    return str;
+  }
+
+  //-- 被っていない数字か判断
+  function new_num_check(tmp, usedCard) {
+    for(i = 0; i < usedCard.length; i++){
+      if( tmp == usedCard[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  //-- 役の判断
+  function card_judge() {
+    
   }
 });
 
@@ -131,7 +193,7 @@ $('#restart').click(function() {
   $("#start").show();
   card_back();
 
-  //-- カードを裏返しに
+  //-- カードを裏返しにし、番号を初期化
   function card_back() {
     for(i = 0;i < 5;i++){
       $("#card" + i).attr("src","./CardImage/card.gif");
--- test_area/js/img-change.js
+++ test_area/js/img-change.js
@@ -6,31 +6,24 @@ $(document).ready( function(){
     alert("Click");
   });
 
-
-
   $('.overimg').on({
       'mouseenter':function(){ // マウスが重なったときの処理
           $(this).fadeTo(500,0.5);
       },
       'mouseleave':function(){ // マウスが外れた時の処理
       $(this).fadeTo(500,1);
+      },
+      'click':function() {
+        var position = $(this).position();
+        if($(this).hasClass('selected')) {  // 外すとき
+          $(this).removeClass("selected");
+          $(this).animate({"top": "+=50px"},  150);
+        } else {                           // 選ぶとき
+          $(this).addClass("selected");
+          $(this).animate({"top": "-=50px"},  150);
+        }
       }
   });
-
-/*
-  //-- マウスオーバーの処理
-  $(function() {
-	var nav = $('.overimg');
-	nav.hover(
-		function(){
-			$(this).fadeTo(500,0.5);
-		},
-		function () {
-			$(this).fadeTo(500,1);
-		}
-	);
-  });
-*/
 });
 
 //--- ボタン1をクリックしたときの処理
@@ -39,10 +32,12 @@ $("#buttton1").click(function() {
   $(".mozi").css("color","#9932cc");  // クラスによる指定
   //$("h1").css("color","#9932cc");    // タグによる指定
   //$('#mozi1').css("color","#9932cc");    // idによる指定
+  //console.log(/*出力したい変数やメッセージ*/);
 });
 
 //--- ボタン2をクリックしたときの処理
 $("#buttton2").click(function() {
-
-
+  //$('#card1').animate({'margin-top':'-=50px'});
+  $('#card1').animate({'top':'+=50px'});
+  //$('#mozi').animate({'marginLeft': '+=500px'});
 });
